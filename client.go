d0f393dd8babbaf90cc70f83a7d42312eded0cec
Veer <73958752+celestix@users.noreply.github.com>
2023-09-24 09:50:39 +0000

    bump up version to beta13

--- client.go
+++ client.go
@@ -24,7 +24,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const VERSION = "v1.0.0-beta10"
+const VERSION = "v1.0.0-beta13"
 
 type Client struct {
 	// Dispatcher handlers the incoming updates and execute mapped handlers. It is recommended to use dispatcher.MakeDispatcher function for this field.
