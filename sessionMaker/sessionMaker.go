59a0c312b2e8c9a87d87a13cf35f1484ae4eafac
KoNekoD <moskva111@yahoo.com>
2024-01-10 23:12:22 +0300

    fix session

--- sessionMaker/sessionMaker.go
+++ sessionMaker/sessionMaker.go
@@ -9,30 +9,32 @@ import (
 	"github.com/gotd/td/telegram"
 )
 
-func NewSessionStorage(ctx context.Context, sessionType SessionConstructor, inMemory bool) (*storage.PeerStorage, telegram.SessionStorage, error) {
+func NewSessionStorage(
+	ctx context.Context,
+	sessionType SessionConstructor,
+	inMemory bool,
+) (*storage.PeerStorage, telegram.SessionStorage, error) {
 	name, data, err := sessionType.loadSession()
 	if err != nil {
-		if err != errSqliteSession {
-			return nil, nil, err
-		}
-		peerStorage := storage.NewPeerStorage(
-			fmt.Sprintf("%s.session", name), false)
-		return peerStorage, &SessionStorage{
-			data:        peerStorage.GetSession().Data,
-			peerStorage: peerStorage,
-		}, nil
+		return nil, nil, err
 	}
-	peerStorage := storage.NewPeerStorage(fmt.Sprintf("%s.session", name), inMemory)
+
 	if inMemory {
 		s := session.StorageMemory{}
 		err := s.StoreSession(ctx, data)
 		if err != nil {
 			return nil, nil, err
 		}
+
+		peerStorage := storage.NewPeerStorage("", inMemory)
 		return peerStorage, &s, nil
 	}
-	return peerStorage, &SessionStorage{
-		data:        data,
-		peerStorage: peerStorage,
-	}, nil
+
+	peerStorage := storage.NewPeerStorage(
+		fmt.Sprintf("%s.session", name),
+		inMemory,
+	)
+	s := SessionStorage{data: data, peerStorage: peerStorage}
+
+	return peerStorage, &s, nil
 }
